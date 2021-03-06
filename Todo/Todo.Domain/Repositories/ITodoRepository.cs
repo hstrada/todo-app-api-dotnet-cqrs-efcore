4bbce2ad7c26e74bb581355d581de921161d7a4e
hstrada <h.strada@hotmail.com>
2021-03-06 11:42:04 -0300

    feat: add dependency injection and repositories

--- Todo/Todo.Domain/Repositories/ITodoRepository.cs
+++ Todo/Todo.Domain/Repositories/ITodoRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 
 namespace Todo.Domain.Repositories
@@ -6,9 +7,16 @@ namespace Todo.Domain.Repositories
     // não quer a aplicacao acoplada, aqui mantém só os contratos para o repositório, repository pattern
     public interface ITodoRepository
     {
+
         // definicao do nosso contrato, abstracao do repositorio, como ele vai salvar, é problema de infra
         void Create(TodoItem todo);
         void Update(TodoItem todo);
         TodoItem GetById(Guid id, string user);
+
+        IEnumerable<TodoItem> GetAll(string user);
+        IEnumerable<TodoItem> GetAllDone(string user);
+        IEnumerable<TodoItem> GetAllUndone(string user);
+        IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done);
+
     }
 }
